feb49b64bc19ccafe2a34fb05753a9a06d28b2e6
Willy <willydias01@gmail.com>
2021-08-23 18:37:32 -0300

    Ajustes4

--- resources/views/adm/_include/contentShowMembro.blade.php
+++ resources/views/adm/_include/contentShowMembro.blade.php
@@ -48,49 +48,45 @@
         <li>Endereço: <span>{{ $membro->endereco }}, {{ $membro->numero }}</span></li>
         <li>Cidade: <span>{{ $membro->cidade }}</span></li>
         <li>UF: <span>{{ $membro->uf }}</span></li>
-
         <li>E-mail: <span> {{ $user->email }}</span></li>
         <li>Telefone: <span>{{ $membro->telefone }} / {{ $membro->celular }}</span></li>
     </fieldset>
     {{-- <fieldset class="p-2 mb-2">
-                <legend class="w-auto m-0 fs-20">Graduação</legend>
+        <legend class="w-auto m-0 fs-20">Graduação</legend>
 
-                <li>Graduação: <span>{{ $membro->graduacao }}</span></li>
-                <li>Universidade: <span>{{ $membro->universidade }}</span></li>
-                <li>Data Formação: <span>{{ $membro->dataFormacao }}</span></li>
-
-            </fieldset> --}}
+        <li>Graduação: <span>{{ $membro->graduacao }}</span></li>
+        <li>Universidade: <span>{{ $membro->universidade }}</span></li>
+        <li>Data Formação: <span>{{ $membro->dataFormacao }}</span></li>
+    </fieldset> --}}
     <fieldset class="p-3 mb-3">
         <legend class="w-auto m-0 fs-20">Documentação</legend>
         <li>
             <span>Ficha </span>
-            <a href="{{ url('/storage/files/') }}/{{ $membro->ncarteirinha }}/{{ $arquivos->ficha }}">Baixar</a>
+            <a target="_blank" href="{{ url('/storage/files/') }}/{{ $membro->ncarteirinha }}/{{ $arquivos->ficha }}">Baixar</a>
         </li>
         <li>
             <span>Diploma </span>
-            <a href="{{ url('/storage/files/') }}/{{ $membro->ncarteirinha }}/{{ $arquivos->diploma }}">Baixar</a>
+            <a target="_blank" href="{{ url('/storage/files/') }}/{{ $membro->ncarteirinha }}/{{ $arquivos->diploma }}">Baixar</a>
         </li>
         <li>
             <span>Diploma verso </span>
-            <a
-                href="{{ url('/storage/files/') }}/{{ $membro->ncarteirinha }}/{{ $arquivos->diploma_verso }}">Baixar</a>
+            <a target="_blank" href="{{ url('/storage/files/') }}/{{ $membro->ncarteirinha }}/{{ $arquivos->diploma_verso }}">Baixar</a>
         </li>
         <li>
             <span>RG </span>
-            <a href="{{ url('/storage/files/') }}/{{ $membro->ncarteirinha }}/{{ $arquivos->rg }}">Baixar</a>
+            <a target="_blank" href="{{ url('/storage/files/') }}/{{ $membro->ncarteirinha }}/{{ $arquivos->rg }}">Baixar</a>
         </li>
         <li>
             <span>CPF </span>
-            <a href="{{ url('/storage/files/') }}/{{ $membro->ncarteirinha }}/{{ $arquivos->cpf }}">Baixar</a>
+            <a target="_blank" href="{{ url('/storage/files/') }}/{{ $membro->ncarteirinha }}/{{ $arquivos->cpf }}">Baixar</a>
         </li>
         <li>
             <span>Título </span>
-            <a href="{{ url('/storage/files/') }}/{{ $membro->ncarteirinha }}/{{ $arquivos->titulo }}">Baixar</a>
+            <a target="_blank" href="{{ url('/storage/files/') }}/{{ $membro->ncarteirinha }}/{{ $arquivos->titulo }}">Baixar</a>
         </li>
         <li>
             <span>Comprovante </span>
-            <a
-                href="{{ url('/storage/files/') }}/{{ $membro->ncarteirinha }}/{{ $arquivos->comprovante }}">Baixar</a>
+            <a target="_blank" href="{{ url('/storage/files/') }}/{{ $membro->ncarteirinha }}/{{ $arquivos->comprovante }}">Baixar</a>
         </li>
     </fieldset>
 </ul>
